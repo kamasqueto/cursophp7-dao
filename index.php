13b05671ba770de0aa605417c551153eac0d302a
kamasqueto <kaiquemasqueto@gmail.com>
2019-07-14 16:45:55 -0300

    metódos de lista

--- index.php
+++ index.php
@@ -8,10 +8,22 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);*/
 
-$root = new Usuarios();
-
+// Carrega um usuário 
+/*$root = new Usuarios();
 $root->loadById(7);
+echo $root;*/
 
-echo $root;
+//Carrega uma lista de usuários
+//$lista = Usuarios::getList();
+//echo json_encode($lista);
 
+
+// Carrega uma lista de usuarios buscando pelo login
+//$busca = Usuarios::search("ka");
+//echo json_encode($busca);	
+
+//Carrega um usuario usando login e a senha
+$usuario = new Usuarios();
+$usuario->login("kamas", "kaique20");
+echo $usuario;
  ?>
